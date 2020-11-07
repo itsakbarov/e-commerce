313e6e41a315112d4c1f26f6acb446c801b27183
theAkbarov <asrorjon5538@gmail.com>
2020-11-07 23:03:55 +0500

    added some features and changes

--- source/src/scripts/script.js
+++ source/src/scripts/script.js
@@ -11,57 +11,29 @@ var Ctor = Vue.extend(Main)
 new Ctor().$mount('#app');
 
 $(document).ready(function() {
-    $('.dropdown_element').hover(function() {
-        $('.dropdown__menu').slideToggle(400)
-    });
-    $('.categories__btn').click(function() {
-        $('.categories__menu').slideToggle(400)
-    });
-    $('.mobile__header').hover(function() {
-        $('.mobile__dropdown').fadeToggle(400)
-    });
-    $('.smartphones').hover(function() {
-        $('.smartPhones__dropdown').fadeToggle(400)
-    });
-    $('.accessories').hover(function() {
-        $('.accessories__dropdown').fadeToggle(600)
-    });
-    $('.retired').hover(function() {
-        $('.retired__dropdown').fadeToggle(600)
-    });
-    $('.owl-carousel').owlCarousel({
+    console.log('document works!!!');
+
+    var owlTeste = $('.owl-carousel');
+
+    owlTeste.owlCarousel({
         loop: true,
-        margin: 10,
-        nav: true,
+        autoplay: true,
         responsive: {
-            0: {
+            1200: {
                 items: 1
             },
-            600: {
-                items: 3
+            960: {
+                items: 1
             },
-            1000: {
-                items: 5
+            425: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            0: {
+                items: 1,
             }
         },
-
-    })
-});
-var swiper = new Swiper('.swiper-container', {
-    speed: 600,
-    parallax: true,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    mousewheel: {
-        invert: true,
-    },
-    autoplay: {
-        delay: 5000
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    },
-});
+    });
+})
